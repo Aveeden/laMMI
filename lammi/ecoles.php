b2f7d71274b989c459b12bc6aae3de5ab8535bfd
Ave <61968475+Aveeden@users.noreply.github.com>
2020-06-07 23:02:38 +0200

    Update ecoles.php

--- lammi/ecoles.php
+++ lammi/ecoles.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styled.css">
     <link rel="stylesheet" href="css/polices.css">
-    <title>Lieux d'études</title>
+    <title>Écoles</title>
 </head>
 <body>
-    <h1>Lieux d'études</h1>
+    <h1>Les Écoles</h1>
 </body>
 </html>
