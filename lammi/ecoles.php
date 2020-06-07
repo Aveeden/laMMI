0f7f72b5c1f07e55aeb722b0cba4097f8313953d
Ave <61968475+Aveeden@users.noreply.github.com>
2020-06-07 23:39:29 +0200

    Update ecoles.php

--- lammi/ecoles.php
+++ lammi/ecoles.php
@@ -1,3 +1,8 @@
+<?php 
+  include('lib/app.php');
+  $db = getDatabase();
+  ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -9,5 +14,22 @@
 </head>
 <body>
     <h1>Les Écoles</h1>
+
+    <?php $sql = "SELECT diplome.*, ecole.* ,(diplome.nom) as intitule 
+FROM `formation`, `diplome`, `ecole` 
+WHERE ecole.id=1 
+AND formation.id_ecole=ecole.id 
+AND formation.id_diplome=diplome.id";
+
+$req = $db -> prepare($sql);
+$req -> execute();
+
+while($data = $req -> fetch()){
+    echo '<p>'.$data['nom'].' '.'<b>'.', disponible ici: '.'</b>'.$data['intitule'].'</p>';
+}
+
+$req = null;
+?>
+
 </body>
 </html>
