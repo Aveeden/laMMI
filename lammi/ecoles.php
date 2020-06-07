53b164042403db15948a327aa3a6a1be4ac16eec
Moumoute73 <32091744+Moumoute73@users.noreply.github.com>
2020-06-07 23:38:20 +0200

    Update ecoles.php

--- lammi/ecoles.php
+++ lammi/ecoles.php
@@ -9,5 +9,9 @@
 </head>
 <body>
     <h1>Les Écoles</h1>
+    
+    <p>Après la MMI , beaucoup d'écoles sont prêtes à acceuilir les tout-nouveaux diplomés pour leurs permettent de poursuivre leurs études.
+        Nombreuse sont ces écoles , donc voici un récapitulatif pour vous permettrent de vous y retrouver !
+    </p>
 </body>
 </html>
