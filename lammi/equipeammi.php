cbed90410b3057a2c138405a9aeb34668d511166
Ave <61968475+Aveeden@users.noreply.github.com>
2020-06-08 16:44:56 +0200

    update

--- lammi/equipeammi.php
+++ lammi/equipeammi.php
@@ -18,51 +18,51 @@
     <?php endif; ?>
     <audio src="sounds/Ambiance.mp3" autoplay loop></audio>
     <a href="menu.html"><img class="logo" src="img/logo-protut.png"></a>
-    <h1>Notre équipe</h1>
-
-    <div class="titre">
-        <p> Julien DOS SANTOS </p>
-        <p> Aubin OLIVRIE </p>
-        <p> Martin GARCIA </p>
-        <p> Adam ZEGHOUD </p>
-    </div>
-
-    <div class="img4">
-        <img src="img/JU.jpg" alt="" class="enligne">
-
-
-        <img src="img/AU.jpg" alt="" class="enligne">
-
-        <img src="img/MG.jpg" alt="" class="enligne">
-        <img src="img/AZ.jpg" alt="" class="enligne">
-
-    </div>
-
-    <section>
-        <div class="J">
-            <a href="https://etudiant.u-pem.fr/~jdossa11/cv-numerique/accueil.html" target="blank">
-                <p> Liens vers CV numérique </p>
-            </a>
-
+        <h1>Notre équipe</h1>
+    
+        <div class="titre">
+            <p> Julien DOS SANTOS </p>
+            <p> Aubin OLIVRIE </p>
+            <p> Martin GARCIA </p>
+            <p> Adam ZEGHOUD </p>
         </div>
-        <div class="A">
-            <a href="https://etudiant.u-pem.fr/~aolivrie/" target="blank">
-                <p> Liens vers CV numérique </p>
-            </a>
-
+    
+        <div class="img4">
+            <img src="img/JU.jpg" alt="" class="enligne">
+    
+    
+            <img src="img/AU.jpg" alt="" class="enligne">
+    
+            <img src="img/MG.jpg" alt="" class="enligne">
+            <img src="img/AZ.jpg" alt="" class="enligne">
+    
         </div>
-        <div class="M">
-            <a href="https://etudiant.u-pem.fr/~mgarci13/" target="blank">
-                <p> Liens vers CV numérique </p>
-            </a>
-
-        </div>
-        <div class="AD">
-            <a href="" target="blank">
-                <p> Liens vers CV numérique </p>
-            </a>
-
-        </div>
+    
+        <section>
+            <div class="J">
+                <a href="https://etudiant.u-pem.fr/~jdossa11/cv-numerique/accueil.html" target="blank">
+                    <p> Liens vers CV numérique </p>
+                </a>
+    
+            </div>
+            <div class="A">
+                <a href="https://etudiant.u-pem.fr/~aolivrie/" target="blank">
+                    <p> Liens vers CV numérique </p>
+                </a>
+    
+            </div>
+            <div class="M">
+                <a href="https://etudiant.u-pem.fr/~mgarci13/" target="blank">
+                    <p> Liens vers CV numérique </p>
+                </a>
+    
+            </div>
+            <div class="AD">
+                <a href="" target="blank">
+                    <p> Liens vers CV numérique </p>
+                </a>
+    
+        </section>
         <div class="container">
             <h2>Contact</h2>
             <div>
